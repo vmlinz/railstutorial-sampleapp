917da3f66ad15b52b028b4044630bad5404f637c
Zaicheng Qi <vmlinz@gmail.com>
2014-10-20 13:52:35 +0000

    Update static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "h1", "Home | Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "h1", "Help | Sample App"
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "h1", "About | Sample App"
   end
 
 end
