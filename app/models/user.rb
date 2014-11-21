9f0273f10200371574d2c281cd94184caa246ce8
Zaicheng Qi <vmlinz@gmail.com>
2014-11-21 10:04:18 +0000

    Finish 11.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    has_many :microposts, dependent: :destroy
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :name, presence: true, length: { maximum: 50 }
@@ -60,6 +61,10 @@ class User < ActiveRecord::Base
     
     def password_reset_expired?
         reset_sent_at < 2.hours.ago
+    end
+    
+    def feed
+        Micropost.where("user_id = ?", id)
     end
     
     private
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -79,4 +79,12 @@ class UserTest < ActiveSupport::TestCase
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
   end
+  
+  test "associated posts should be deleted when user deleted" do
+    @user.save
+    @user.microposts.create!(content: "a test post")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 end
